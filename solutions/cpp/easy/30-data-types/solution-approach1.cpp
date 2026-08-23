8a271765b87efae37e8bf65d24a56b07113b7f53
Rafid Ahmed Prince <rafidaprince@gmail.com>
2026-08-24 01:40:24 +0600

    ✅ Day 1: Data Types [cpp] — approach1

---
+++ solutions/cpp/easy/30-data-types/solution-approach1.cpp
@@ -0,0 +1,22 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/30-data-types/problem?isFullScreen=true
+// Problem     Day 1: Data Types
+// Difficulty  Easy
+// Subdomain   30 Days of Code
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-08-24, 01:40 a.m.
+// ──────────────────────────────────────────────────
+
+
+
+    int a ; double b ;  string c ;
+    cin >> a ;
+    cin >> b ;
+    cin.ignore();
+    getline(cin , c);
+    cout << i + a << endl;
+    cout <<fixed << setprecision(1) << d + b << endl;
+    cout << s+c << endl;
+
