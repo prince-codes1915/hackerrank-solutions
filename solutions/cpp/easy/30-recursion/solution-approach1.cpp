cb19f68270bf9c2c8617541ee7e6a2cde4636e8a
Rafid Ahmed Prince <rafidaprince@gmail.com>
2026-08-31 22:50:43 +0600

    ✅ Day 9: Recursion 3   [cpp] — approach1

---
+++ solutions/cpp/easy/30-recursion/solution-approach1.cpp
@@ -0,0 +1,69 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/30-recursion/problem?isFullScreen=true
+// Problem     Day 9: Recursion 3  
+// Difficulty  Easy
+// Subdomain   30 Days of Code
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-08-31, 10:50 p.m.
+// ──────────────────────────────────────────────────
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'factorial' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+int factorial(int n) {
+    if(n<2) return 1;
+    else return n * factorial(n-1);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    int result = factorial(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
