65f8c38444d991e1e9e06d906c69dfb5b0c3cfd4
Rafid Ahmed Prince <rafidaprince@gmail.com>
2026-09-06 03:25:00 +0600

    ✅ Mini-Max Sum [cpp] — approach1

---
+++ solutions/cpp/easy/mini-max-sum/solution-approach1.cpp
@@ -0,0 +1,110 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/mini-max-sum/problem?isFullScreen=true
+// Problem     Mini-Max Sum
+// Difficulty  Easy
+// Subdomain   Warmup
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-09-06, 03:24 a.m.
+// ──────────────────────────────────────────────────
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+void miniMaxSum(vector<int> arr) {
+    long max = 0;
+    long min = LLONG_MAX;
+    for(int i = 0 ; i < arr.size() ; i++)
+    {
+        long temp = 0;
+        for(int j = 0; j < arr.size() ; j++)
+        {
+            if(j==i) continue;
+            else{
+                temp += arr[j];
+            }
+        }
+        if(temp > max)
+        {
+            max = temp;
+        }
+        if (temp < min)
+        {
+            min = temp;
+        }
+        
+    }
+    cout << min << " " << max << endl;
+}
+
+int main()
+{
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(5);
+
+    for (int i = 0; i < 5; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    miniMaxSum(arr);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
